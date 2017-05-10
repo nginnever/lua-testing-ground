89de0452e2c01f3adf2b487ee0b1e67f8edaa4f1
nginnever <ginneversource@gmail.com>
2017-05-10 14:17:55 -0700

    conditionals

---
+++ basics/conditionals.lua
@@ -0,0 +1,10 @@
+age = 31
+
+if age < 31 then
+  io.write("You is getting old nigga", "\n")
+  local localVar = 10
+elseif (age <= 40) and (age < 35) then
+  io.write("blah blah", "\n")
+else
+  io.write("somethng else", "\n")
+end
